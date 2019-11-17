2bd844dec656a1c6c2fdbf8dd3918f8ab615522d
matkuc003 <mnorris.k97@gmail.com>
2019-11-17 23:54:50 +0100

    Added character

---
+++ Assets/PlayerControll.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    float speed = 40f;
+    float moveHor = 0f;
+    bool jump = false;
+    bool crouch = false;
+    // Update is called once per frame
+    void Update()
+    {
+        moveHor = Input.GetAxis("Horizontal") * speed;
+        animator.SetFloat("Speed", Mathf.Abs(moveHor));
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            animator.SetBool("IsCrouching", true);
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            animator.SetBool("IsCrouching", false);
+            crouch = false;
+        }
+    }
+   public void OnLanding()
+    {
+
+        animator.SetBool("IsJumping", false);
+    }
+    public void OnCrouching(bool isCrouching)
+    {
+
+        animator.SetBool("IsCrouching", isCrouching);
+
+    }
+
+   public void FixedUpdate()
+    {
+        controller.Move(moveHor * Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+    }
+}
+
