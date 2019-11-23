1ffc9738716b2bd5494508b11061e6c1abd0a023
mbalcer <mateusz75319@gmail.com>
2019-11-23 22:26:13 +0100

    Fixed playing music

--- Assets/Scripts/SoundManagerScript.cs
+++ Assets/Scripts/SoundManagerScript.cs
@@ -4,39 +4,31 @@ using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
-    public static AudioClip fallSound, jumpSound, completeSound, pickUpSound, hitSound, menuSound;
+    public static AudioClip fallSound, jumpSound, completeSound, pickUpSound, hitSound, menuSound, music;
     public static AudioSource audioSrc;
-    private static bool createdSound= false;
-    // Start is called before the first frame update
-    void Awake()
-    {
-        if (createdSound == false)
-        {
-            DontDestroyOnLoad(this.gameObject);
-            createdSound = true;
-        };
-     
-          // Jak to się odkomentuje to przy przejsciu z menu do innej sceny ten skrypt zostaje, jakby przekazywany jest dalej, ale jak się cofnie z levels do menu to skrypt się duplikuje
-          // i dzwieki w tle sie nakladaja
-          // Do każdej sceny teraz skopiowałem SoundManager(asset) - to jest jakies rozwiazanie ale muzyka za kazdym przejściem między scenami zaczyna się od nowa
-          // Jak ktoś ma jakiś sposób jak to naprawić to śmiało
-        
-        
+    private static bool createdSound = false;
     
-    }
     void Start()
     {
+        
         fallSound = Resources.Load<AudioClip>("Fall");
         jumpSound = Resources.Load<AudioClip>("Jump2");
         completeSound = Resources.Load<AudioClip>("levelComplete");
         pickUpSound = Resources.Load<AudioClip>("PickUp");
         hitSound = Resources.Load<AudioClip>("hit");
         menuSound = Resources.Load<AudioClip>("Menu2");
+        music = Resources.Load<AudioClip>("Tiny_Blocks");
         audioSrc = GetComponent<AudioSource>();
+        if (createdSound == false)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            createdSound = true;
+            audioSrc.loop = true;
+            audioSrc.Play();
+        };
     }
 
     // Update is called once per frame
-
 
     void Update()
     {
