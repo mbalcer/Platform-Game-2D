1a0b184ef5df4374c665a8a9691019c00e2e9799
Szymon1109 <szymonbetlewski@wp.pl>
2019-11-23 02:15:41 +0100

    Added displaying number of gained coins

--- Assets/Scripts/CoinManagerScript.cs
+++ Assets/Scripts/CoinManagerScript.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinManagerScript : MonoBehaviour
 {
-    private float money;
+    private float money = 0;
+    public Text coins;
 
     public void CollectedCoin()
     {
         money += 1;
+
+        coins = GameObject.Find("Coins").GetComponent<Text>();
+        coins.text = money.ToString();
     }
 }
